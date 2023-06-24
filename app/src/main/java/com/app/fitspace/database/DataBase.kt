c911b50101c5b74510c6a23255878d3a11b6e4c3
Bernardo Oechsler <bernardo.oechsler6@gmail.com>
2023-06-24 03:31:28 -0700

    Criacao do companion object com fun getDataBaseInstance()

--- app/src/main/java/com/app/fitspace/database/DataBase.kt
+++ app/src/main/java/com/app/fitspace/database/DataBase.kt
@@ -1,10 +1,28 @@
 package com.app.fitspace.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [UserEntity::class], version = 1)
-abstract class DataBase: RoomDatabase() {
+abstract class DataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE: DataBase? = null
+
+        fun getDataBaseInstance(context: Context): DataBase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext, DataBase::class.java, "user_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
 }
