24873d125bd8cfb6d2ad2a208e30348f6ba75d66
withcarlox <withcarlox@gmail.com>
2023-06-26 13:30:32 +0100

    refactor: set version 11

--- app/src/main/java/com/app/fitspace/repository/UserRepository.kt
+++ app/src/main/java/com/app/fitspace/repository/UserRepository.kt
@@ -1,12 +1,11 @@
 package com.app.fitspace.repository
 
 import com.app.fitspace.database.DataBase
-import com.app.fitspace.database.UserDao
 import com.app.fitspace.database.UserEntity
 
 class UserRepository(private val db: DataBase) {
 
-    suspend fun insertUser(user: UserEntity) = db.userDao().insert(user)
+     suspend fun insertUser(user: UserEntity) = db.userDao().insert(user)
 
     suspend fun updateUser(user: UserEntity) = db.userDao().update(user)
 
