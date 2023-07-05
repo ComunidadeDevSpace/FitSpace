a5504a6bc9e084b452687009e54fb058359ce182
jaqviana <jaquelineviana84@gmail.com>
2023-07-05 16:03:47 -0400

    style: splashscreen implementation

--- app/src/main/java/com/app/fitspace/ui/MainActivity.kt
+++ app/src/main/java/com/app/fitspace/ui/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: UserViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
