2493d378ad4869500b9d494da54aea48c3607bcf
Jeferson Barros <im.jbalves@gmail.com>
2023-07-11 03:49:37 -0300

    feat:    - Add Clean Architecture folder with MVVM;    - Add Repository Pattern    - Adjust UserViewModel & Appdatabse

--- app/src/main/java/com/app/fitspace/network/Network.kt
+++
@@ -1,4 +0,0 @@
-package com.app.fitspace.network
-
-class Network {
-}
---
+++ app/src/main/java/com/app/fitspace/presentation/viewmodel/UserViewModel.kt
@@ -0,0 +1,39 @@
+package com.app.fitspace.presentation.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.app.fitspace.data.local.AppDatabase
+import com.app.fitspace.data.model.User
+import com.app.fitspace.domain.repository.UserRepository
+import com.app.fitspace.data.repository.UserRepositoryImpl
+import kotlinx.coroutines.launch
+
+class UserViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+
+    private val userRepository: UserRepository
+    init {
+        val userDao = AppDatabase.getInstance(application).userDao()
+        userRepository = UserRepositoryImpl(userDao)
+    }
+
+    fun insertUser(user: User) {
+        viewModelScope.launch {
+            userRepository.insertUser(user)
+        }
+    }
+
+    fun updateUser(user: User) {
+        viewModelScope.launch {
+            userRepository.updateUser(user)
+        }
+    }
+
+    fun getUserByEmail(email: String) {
+        viewModelScope.launch {
+            userRepository.getUserByEmail(email)
+        }
+    }
+}
