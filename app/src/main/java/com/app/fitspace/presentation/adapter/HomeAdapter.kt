4873b6138d8028c7ef1af14ed57345ef69a04409
Sergio Bismark Cim Marchewsky <sergiobismark06@gmail.com>
2023-07-24 18:22:12 -0300

    Added BotomNavigationBar and fragments

---
+++ app/src/main/java/com/app/fitspace/presentation/adapter/HomeAdapter.kt
@@ -0,0 +1,1 @@
+package com.app.fitspace.presentation.adapter
--- app/src/main/java/com/app/fitspace/presentation/view/MainActivity.kt
+++ app/src/main/java/com/app/fitspace/presentation/view/MainActivity.kt
@@ -1,45 +1,56 @@
 package com.app.fitspace.presentation.view
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
-import com.app.fitspace.data.model.User
-import com.app.fitspace.databinding.ActivityMainBinding
-import com.app.fitspace.presentation.viewmodel.UserViewModel
+import android.view.Menu
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.app.fitspace.R
+import com.app.fitspace.databinding.FragmentHomeBinding
+import com.app.fitspace.presentation.fragment.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var userViewModel: UserViewModel
-    //private val userViewModel: UserViewModel by viewModels()
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var fragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        val factory = ViewModelProvider.AndroidViewModelFactory.getInstance(application)
-        userViewModel = ViewModelProvider(this,factory).get(UserViewModel::class.java)
+        bottomNavigationView = findViewById(R.id.bottom_nav_view)
+        fragmentManager = supportFragmentManager
 
-        /**
-         * Example how-to use userViewModel to input data
-         * 1 - created a user instance with your data
-         * 2 - called ViewModel with the respective method and put the user object
-         */
-        val user = User(
-            "Jeferson Barros",
-            "im.jbalves@gmail.com",
-            "jeff",
-            "123456",
-            "+554199999-9999",
-            "16/09/1985",
-            "male"
-            )
-        userViewModel.insertUser(user)
-
-        binding.saveBtn.setOnClickListener {
-            val intent = Intent(this, SignUp::class.java)
-            startActivity(intent)
+        // Define o ouvinte para o evento de seleção de item do BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            // Chama o método para trocar o fragmento
+            switchFragment(item.itemId)
+            true
         }
+
+        // Exibe o fragmento inicial
+        switchFragment(R.id.fragment_home)
+
     }
+
+    private fun switchFragment(itemId: Int) {
+        val fragment: Fragment = when (itemId) {
+            R.id.fragment_home -> Fragment()
+            R.id.fragment_stats -> Fragment()
+            else -> Fragment()
+        }
+
+        // Realiza a transação do fragmento para exibir o selecionado
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.bottom_app_bar, fragment)
+        fragmentTransaction.commit()
+    }
+
+
+
 }
