4f815d736933afab8c610e6c2a20326ae3f63181
Beatriz <beatriz_bagesteiro@hotmail.com>
2023-09-02 10:49:12 -0300

    Goals viewModel

--- app/src/main/java/com/app/fitspace/presentation/viewmodel/UserGoalsViewModel.kt
+++ app/src/main/java/com/app/fitspace/presentation/viewmodel/UserGoalsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.app.fitspace.FitSpaceApplication
 import com.app.fitspace.data.local.UserGoalsDao
 import com.app.fitspace.data.model.UserGoals
 import com.app.fitspace.utils.ActionTypeGoals
@@ -14,10 +15,44 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class UserGoalsViewModel(
-    private val userGoalsDao: UserGoalsDao,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO)
- : ViewModel(){
+    private val userGoalsDao: UserGoalsDao)
+    : ViewModel(){
 
 
-  
+    fun execute(goalsAction: GoalsAction){
+        when (goalsAction.actionType){
+            ActionTypeGoals.INSERT.name -> insert(goalsAction.userGoals!!)
+            ActionTypeGoals.UPDATE.name -> update(goalsAction.userGoals!!)
+        }
+    }
+
+
+    fun insert(userGoals: UserGoals){
+        viewModelScope.launch {
+            userGoalsDao.insertGoals(userGoals)
+        }
+    }
+
+    fun update(userGoals: UserGoals){
+        viewModelScope.launch {
+            userGoalsDao.updateGoals(userGoals)
+        }
+    }
+
+
+
+    companion object {
+        fun getVmFactory(application: Application): ViewModelProvider.Factory {
+            val dataBaseInstance = (application as FitSpaceApplication).getAppDataBase()
+            val dao  = dataBaseInstance.userGoalsDao()
+
+            return object : ViewModelProvider.Factory{
+                override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+                    return UserGoalsViewModel(dao) as T
+                }
+            }
+        }
+    }
+
+
 }
