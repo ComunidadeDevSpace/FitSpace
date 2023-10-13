f8f3bcd835e2c0501f6e961d0cb99187e93be404
vhboico <hboico@hotmail.com>
2023-10-13 07:56:38 -0300

    chore: exit function

--- app/src/main/java/com/app/fitspace/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/app/fitspace/presentation/fragment/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.app.fitspace.presentation.fragment
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import com.app.fitspace.R
 import com.app.fitspace.presentation.view.SignUp
+import kotlin.system.exitProcess
 
 class SettingsFragment : Fragment() {
 
@@ -18,6 +20,12 @@ class SettingsFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_settings, container, false)
         val cardViewProfile = rootView.findViewById<CardView>(R.id.card_view_item_profile)
+
+        val imgBtn = rootView.findViewById<CardView>(R.id.card_view_item_logout)
+
+        imgBtn.setOnClickListener {
+            showAlertDialog()
+        }
 
         cardViewProfile.setOnClickListener {
             // Iniciando a atividade de edição do perfil
@@ -33,4 +41,19 @@ class SettingsFragment : Fragment() {
         fun newInstance() =
             SettingsFragment()
     }
+
+    private fun showAlertDialog() {
+        val alertDialog = AlertDialog.Builder(requireContext(), R.style.DialogTheme)
+            .setTitle("Tem certeza que deseja sair?")
+            .setMessage("Você precisará logar novamente")
+            .setPositiveButton("Sim") { _, _ ->
+                exitProcess(0)
+            }
+            .setNegativeButton("Não") { _, _ -> }
+            .create()
+        alertDialog.show()
+    }
 }
+
+
+
