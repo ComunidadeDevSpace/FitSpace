a4f76636a6e4c5609fae6ea5a742a1a63cf13d25
Sergio Bismark Cim Marchewsky <sergiobismark06@gmail.com>
2023-10-22 22:02:18 -0300

    - StatusBar isn't responsive with themes yet, let it black for both.
    - SettingsFragment now has icons responsive to selected theme.
    
    - SiginIn materialCardviews margin fixed

--- app/src/main/java/com/app/fitspace/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/app/fitspace/presentation/fragment/SettingsFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import com.app.fitspace.R
 import com.app.fitspace.presentation.view.SignUp
@@ -40,6 +41,39 @@ class SettingsFragment : Fragment() {
         }
 
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val profileIcon: ImageView = view.findViewById(R.id.image_view_item_profile)
+        val notificationsIcon: ImageView = view.findViewById(R.id.image_view_item_notifications)
+        val languageIcon: ImageView = view.findViewById(R.id.image_view_item_language)
+        val darkModeIcon: ImageView = view.findViewById(R.id.image_view_item_dark_mode)
+        val logoutIcon: ImageView = view.findViewById(R.id.image_view_item_logout)
+        val infoIcon: ImageView = view.findViewById(R.id.image_view_item_info)
+
+        if (isDarkTheme()) {
+            profileIcon.setImageResource(R.drawable.edit_dark_mode)
+            notificationsIcon.setImageResource(R.drawable.notification_dark_mode)
+            languageIcon.setImageResource(R.drawable.language_dark_mode)
+            darkModeIcon.setImageResource(R.drawable.dark_mode_dark_mode)
+            logoutIcon.setImageResource(R.drawable.logout_dark_mode)
+            infoIcon.setImageResource(R.drawable.info_dark_mode)
+        } else {
+            profileIcon.setImageResource(R.drawable.edit_light_mode)
+            notificationsIcon.setImageResource(R.drawable.notification_light_mode)
+            languageIcon.setImageResource(R.drawable.language_light_mode)
+            darkModeIcon.setImageResource(R.drawable.dark_mode_light_mode)
+            logoutIcon.setImageResource(R.drawable.logout_light_mode)
+            infoIcon.setImageResource(R.drawable.info_light_mode)
+        }
+    }
+
+    private fun isDarkTheme(): Boolean {
+        val nightModeFlags = resources.configuration.uiMode and
+                android.content.res.Configuration.UI_MODE_NIGHT_MASK
+        return nightModeFlags == android.content.res.Configuration.UI_MODE_NIGHT_YES
     }
 
     companion object {
